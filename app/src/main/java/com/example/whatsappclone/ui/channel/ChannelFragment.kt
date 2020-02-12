2545e52488833d1972037d3bab3021d9e612a288
sorianog <13261711+sorianog@users.noreply.github.com>
2020-02-11 19:01:44 -0500

    Worked on ChannelFragment

--- app/src/main/java/com/example/whatsappclone/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/example/whatsappclone/ui/channel/ChannelFragment.kt
@@ -2,12 +2,11 @@ package com.example.whatsappclone.ui.channel
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.whatsappclone.R
@@ -17,11 +16,66 @@ import com.getstream.sdk.chat.rest.User
 import com.getstream.sdk.chat.view.MessageListView
 import com.getstream.sdk.chat.viewmodel.ChannelViewModel
 import com.getstream.sdk.chat.viewmodel.ChannelViewModelFactory
+import kotlinx.android.synthetic.main.fragment_channel.*
 
 
 class ChannelFragment : Fragment() {
 
-    // TODO
+    private val args: ChannelFragmentArgs by navArgs()
+    lateinit var binding: FragmentChannelBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_channel, container, false)
+        return binding.root
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_channel, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            findNavController().navigateUp()
+            return true
+        }
+        return false
+    }
+
+    // Toolbar and ViewModel setup
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity : AppCompatActivity = activity as AppCompatActivity
+
+        activity.setSupportActionBar(binding.toolbar)
+        activity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        activity.supportActionBar!!.setDisplayShowHomeEnabled(true)
+        activity.supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        val client = StreamChat.getInstance(activity.application)
+        val view = view
+        binding.lifecycleOwner = this
+        val channel = client.channel(args.channelType, args.channelId)
+        val factory = ChannelViewModelFactory(activity.application, channel)
+        val viewModel: ChannelViewModel by viewModels { factory }
+
+        binding.viewModel = viewModel
+        binding.messageList.setViewModel(viewModel, this)
+        // binding.messageInputView.setViewModel(viewModel, this)
+
+        val messageListView: MessageListView = view!!.findViewById(R.id.messageList)
+
+        val otherUsers: List<User> = channel.channelState.otherUsers
+        binding.avatarGroup.setChannelAndLastActiveUsers(channel, otherUsers, messageList.style)
+        binding.channelName.text = channel.name
+    }
 }
